34d1992ff8f31a35ae681675448dbcbfef4bb152
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-07-31 22:39:35 +0200

    Add and run prettier

--- src/googleMaps.ts
+++ src/googleMaps.ts
@@ -1,54 +1,51 @@
-import { Loader } from '@googlemaps/js-api-loader';
+import { Loader } from '@googlemaps/js-api-loader'
 
-type GoogleMapsLocation = { id?: string; text: string };
+type GoogleMapsLocation = { id?: string; text: string }
 
 const loader = new Loader({
   apiKey: '',
   libraries: ['places'],
-});
+})
 
-let globalMaps: typeof google.maps;
+let globalMaps: typeof google.maps
 const getMaps = async () => {
-  if (typeof window === 'undefined') return undefined;
-  if (globalMaps) return globalMaps;
+  if (typeof window === 'undefined') return undefined
+  if (globalMaps) return globalMaps
 
-  const google = await loader.load();
+  const google = await loader.load()
 
-  globalMaps = google.maps;
+  globalMaps = google.maps
 
-  return globalMaps;
-};
+  return globalMaps
+}
 
 const googleMaps = {
   getCoordinates: async (address: string) => {
-    const maps = await getMaps();
+    const maps = await getMaps()
 
-    if (!maps) return undefined;
+    if (!maps) return undefined
 
     return new Promise<{ lat: number; lng: number } | undefined>((resolve) => {
-      const geocoder = new maps.Geocoder();
+      const geocoder = new maps.Geocoder()
 
       geocoder.geocode({ address }, (results) => {
         if (!results) {
-          resolve(undefined);
-          return;
+          resolve(undefined)
+          return
         }
-        const { lat, lng } = results[0].geometry.location;
-        resolve({ lat: lat(), lng: lng() });
-      });
-    });
+        const { lat, lng } = results[0].geometry.location
+        resolve({ lat: lat(), lng: lng() })
+      })
+    })
   },
 
-  getDistance: async (
-    from: { lat: number; lng: number },
-    to: { lat: number; lng: number }
-  ) => {
-    const maps = await getMaps();
+  getDistance: async (from: { lat: number; lng: number }, to: { lat: number; lng: number }) => {
+    const maps = await getMaps()
 
-    if (!maps) return undefined;
+    if (!maps) return undefined
 
     return new Promise<number | undefined>((resolve) => {
-      const service = new maps.DistanceMatrixService();
+      const service = new maps.DistanceMatrixService()
 
       service.getDistanceMatrix(
         {
@@ -59,16 +56,16 @@ const googleMaps = {
         },
         (response) => {
           if (!response) {
-            resolve(undefined);
-            return;
+            resolve(undefined)
+            return
           }
-          const { distance } = response.rows[0].elements[0];
-          resolve(distance.value);
-        }
-      );
-    });
+          const { distance } = response.rows[0].elements[0]
+          resolve(distance.value)
+        },
+      )
+    })
   },
-};
+}
 
-export default googleMaps;
-export { type GoogleMapsLocation };
+export default googleMaps
+export { type GoogleMapsLocation }
--- src/useSortByNearest.tsx
+++ src/useSortByNearest.tsx
@@ -1,59 +1,54 @@
-import { useEffect, useState } from 'react';
-import googleMaps from './googleMaps';
+import { useEffect, useState } from 'react'
+import googleMaps from './googleMaps'
 
 type Point = {
-  city: string;
-  lat: number;
-  lng: number;
-  distance: number;
-};
+  city: string
+  lat: number
+  lng: number
+  distance: number
+}
 
 type Props = {
-  location: string;
-  targets: string[];
-};
+  location: string
+  targets: string[]
+}
 
 const useSortByNearest = ({ location, targets }: Props) => {
-  const [sorted, setSorted] = useState<Point[]>([]);
+  const [sorted, setSorted] = useState<Point[]>([])
 
   useEffect(() => {
     const sortDestinations = async () => {
       if (!location || !targets.length) {
-        return;
+        return
       }
 
-      const startingPoint = await googleMaps.getCoordinates(location ?? '');
+      const startingPoint = await googleMaps.getCoordinates(location ?? '')
 
       const targetsWithCoordinates = await Promise.all(
         targets.map(async (item) => {
-          const coordinates = await googleMaps.getCoordinates(item);
-          return { city: item, ...coordinates };
-        })
-      );
+          const coordinates = await googleMaps.getCoordinates(item)
+          return { city: item, ...coordinates }
+        }),
+      )
 
       if (startingPoint && targetsWithCoordinates) {
         const sorted = await Promise.all(
           targetsWithCoordinates.map(async (item) => {
-            const distance = await googleMaps.getDistance(
-              { lat: item.lat || 0, lng: item.lng || 0 },
-              startingPoint
-            );
-            return { ...item, distance };
-          })
+            const distance = await googleMaps.getDistance({ lat: item.lat || 0, lng: item.lng || 0 }, startingPoint)
+            return { ...item, distance }
+          }),
         )
-          .then((results) =>
-            results.filter((result) => result.distance !== undefined)
-          )
+          .then((results) => results.filter((result) => result.distance !== undefined))
           .then((results) => results as Point[])
-          .then((results) => results.sort((a, b) => a.distance - b.distance));
+          .then((results) => results.sort((a, b) => a.distance - b.distance))
 
-        setSorted(sorted);
+        setSorted(sorted)
       }
-    };
-    sortDestinations();
-  }, [location, targets]);
+    }
+    sortDestinations()
+  }, [location, targets])
 
-  return sorted;
-};
+  return sorted
+}
 
-export default useSortByNearest;
+export default useSortByNearest
