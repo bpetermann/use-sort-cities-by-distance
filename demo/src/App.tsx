b4a1dea3a785b2d44ab1c1677ae081d515de05ca
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-08-03 23:30:49 +0200

    Change demo page

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
 import React from 'react'
-import './App.css'
+import { useState } from 'react'
 import cities from './cities.json'
 import { useSortCitiesByDistance } from 'use-sort-cities-by-distance'
+
+const addCities = ['Paris', 'Undefined', 'Barcelona', 'Shanghai', 'Dublin']
 
 function App() {
   const [config, setConfig] = useState({
     list: cities,
     start: 'Vienna',
-    targets: ['London', 'Amsterdam', 'Vienna', 'Berlin', 'Los Angeles', 'Paris', 'Barcelona', 'Shanghai', 'Dublin'],
+    targets: ['London', 'Amsterdam', 'Vienna', 'Berlin', 'Los Angeles'],
   })
 
   const { sorted } = useSortCitiesByDistance(config)
 
   return (
     <>
+      <header>
+        <h2>useSortCitiesByDistance</h2>
+      </header>
       <div className='card'>
+        <h3>
+          <span>Start:</span> {config.start}
+        </h3>
         <input
+          type='text'
           onChange={(e) => {
             setConfig((prev) => ({ ...prev, start: e.target.value }))
           }}
         />
-        <h2>Start: {config.start}</h2>
 
         {sorted?.map((item) => (
           <div key={item.city}>
             <p>
-              {item.city} - {item.distance} km
+              <span>{item.city}</span> - {item.distance} km
             </p>
+          </div>
+        ))}
+      </div>
+
+      <div className='add'>
+        Add:
+        {addCities?.map((item) => (
+          <div key={item}>
+            {!config.targets.includes(item) && (
+              <button
+                onClick={() => {
+                  setConfig({ ...config, targets: [...config.targets, item] })
+                }}
+              >
+                {item}
+              </button>
+            )}
           </div>
         ))}
       </div>
